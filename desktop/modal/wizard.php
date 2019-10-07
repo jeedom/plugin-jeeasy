3f48440e0797d28c8db7c081059f4f2c7120b595
Rocket13011 <alexandre@jeedom.com>
2019-10-07 21:45:37 +0200

    add close to save

--- desktop/modal/wizard.php
+++ desktop/modal/wizard.php
@@ -287,7 +287,7 @@ $('.saveDiv').click( function() {
                 $('#div_alertFirstUse').showAlert({message: '{{Sauvegarde réussie}}', level: 'success'});
             }
         });
-  $('#md_modal').dialog('close');
+  $('#md_modal').load('index.php?v=d&plugin=jeeasy&modal=wizard').dialog('open');
 });
 
 
