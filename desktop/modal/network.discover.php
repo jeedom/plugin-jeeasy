e62599c57a2bc080ca9ae5ed9774aabfa7971283
Sekiro-kost <julien@jeedom.com>
2021-10-22 14:44:42 +0200

    New UI NetworkDiscover

--- desktop/modal/network.discover.php
+++ desktop/modal/network.discover.php
@@ -2,7 +2,14 @@
 if (!isConnect()) {
   throw new Exception('{{401 - Accès non autorisé}}');
 }
+
 $discovers = jeeasy::discoverNetwork();
+$jsonrpc = repo_market::getJsonRpc();
+$marketURL = config::byKey('market::address');
+
+
+
+
 ?>
 <table class="table table-condensed">
   <thead>
@@ -16,8 +23,8 @@ $discovers = jeeasy::discoverNetwork();
   <tbody>
     <?php
     foreach ($discovers as $mac => $value) {
-      echo '<tr>';
-      echo '<td>';
+      echo '<tr height="50%">';
+      echo '<td >';
       echo $value['ip'];
       echo '</td>';
       echo '<td>';
@@ -28,11 +35,23 @@ $discovers = jeeasy::discoverNetwork();
       echo '</td>';
       echo '<td>';
       foreach ($value['plugin'] as $plugin) {
+        $arrId = array();
+        $arrId['id'] = $plugin['id'];
+        if ( $jsonrpc->sendRequest('market::byId', $arrId)) {
+             $result = $jsonrpc->getResult();
+             $finalCost = '';
+             if($result['cost'] > 0){
+               $finalCost = '('.$result['cost'].'.00 €)';
+             }
+            /* log::add('gestAccess','debug','KIKOO'.json_encode($result));*/
+             $imgPlugin = $result['img'];
+
+        }
         $class="btn-info";
         if($plugin['certification'] == 'official'){
           $class="btn-success";
         }
-        echo '<a class="btn '.$class.' btn-xs" href="https://www.jeedom.com/market/index.php?v=d&p=market&type=plugin&plugin_id='.$plugin['id'].'" target="_blank"><i class="fas fa-external-link-alt"></i> '.$plugin['name'].'</a> ';
+        echo '<a class="btn '.$class.' btn-xs" href="https://www.jeedom.com/market/index.php?v=d&p=market&type=plugin&plugin_id='.$plugin['id'].'" target="_blank" ><img class="test" src="'.$marketURL.'/'.$imgPlugin["icon"].'" style="height:20px; margin-bottom:5px; width:20px;border:1px solid black;"> '.$plugin['name'].'  '.$finalCost.'    '.'</a> ';
       }
       echo '</td>';
       echo '</tr>';
