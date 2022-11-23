5cfb9dd72ef4176c8da800ca6692f24d8f3b9d80
Sekiro-kost <julien@jeedom.com>
2022-11-23 16:06:26 +0100

    new urlLinks for services && changeLanguages

--- desktop/modal/assistants.php
+++ desktop/modal/assistants.php
@@ -2,6 +2,15 @@
 if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
+
+
+$arrayLanguages = ['fr_FR','en_US','es_ES','de_DE' ];
+$actualLanguage = config::byKey('language');
+if(!in_array($actualLanguage, $arrayLanguages)){
+    $actualLanguage = 'en_US';
+}
+
+
 ?>
 <div class="col-md-12 text-center"><h2>{{Les assistants vocaux}}</h2></div>
 <div class="col-xs-18 col-sm-12 col-md-6 col-md-offset-3">
@@ -9,11 +18,11 @@ if (!isConnect()) {
         <div class="caption">
             <h4>{{Assistants vocaux}}</h4>
             <p></p>
-            <center><a href="https://jeedom.github.io/documentation/howto/fr_FR/assistant_vocaux_cloud" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> {{Documentation}}</a></center>
+            <center><a href="https://doc.jeedom.com/<?= $actualLanguage; ?>/howto/assistant_vocaux_cloud" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> {{Documentation}}</a></center>
             <p></p>
             <p>{{Nous vous proposons une connexion facile pour profiter des assistants vocaux : Amazon Alexa et Google Assistant}}</p>
             <p></p>
-            <center><a href="https://jeedom.github.io/plugin-gsh/fr_FR/" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> Google Assistant</a> <a href="https://jeedom.github.io/plugin-ash/fr_FR/" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> Amazon Alexa</a></center>
+            <center><a href="https://doc.jeedom.com/<?= $actualLanguage; ?>/plugins/communication/gsh/" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> Google Assistant</a> <a href="https://doc.jeedom.com/<?= $actualLanguage; ?>/plugins/communication/ash/" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> Amazon Alexa</a></center>
         </div>
     </div>
 </div>
