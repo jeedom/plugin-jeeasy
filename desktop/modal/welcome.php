58a8eab9df7b019329dc6c9f69de36bee655281a
Sekiro-kost <julien@jeedom.com>
2023-01-06 14:07:07 +0100

    new advertise on welcomeModale

--- desktop/modal/welcome.php
+++ desktop/modal/welcome.php
@@ -7,6 +7,8 @@ jeeasy::initStartBox();
 
 ?>
 
+
+
 <div class="col-md-12 text-center">
 	<h2>{{Guide de démarrage}} <?php echo config::byKey('product_name'); ?></h2>
 </div>
@@ -14,4 +16,8 @@ jeeasy::initStartBox();
 <div class="col-md-12 text-center">
 	<p class="text-center"><br />{{Ce guide va vous aider à configurer votre}} <?php echo config::byKey('product_name'); ?> {{en quelques étapes.}}</p>
 	<p class="text-center"><br />{{Cliquez sur la flèche pour commencer la configuration.}}</p>
+</div>
+<div class="col-md-12 text-center">
+	<h4 style="color:red;">IL EST IMPORTANT DE TERMINER CETTE CONFIGURATION : plusieurs plugins essentiels seront installés durant le processus.</h4>
+	<h4>Ne fermez pas cette fenetre jusqu'a son terme.</h4>
 </div>
