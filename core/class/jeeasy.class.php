c9293a8e0f8eb66f6710bbcf1f5d647337a5fae4
Sekiro-kost <julien@jeedom.com>
2023-01-17 15:08:47 +0100

    add sleep checkplugin

--- core/class/jeeasy.class.php
+++ core/class/jeeasy.class.php
@@ -379,8 +379,9 @@ class jeeasy extends eqLogic {
 		repo_market::test();
 		try{
           	jeeasy::checkPlugin('openvpn');
+		sleep(10);
           	config::save('market::allowDNS',1);
-			network::dns_start();
+		network::dns_start();
 		}catch (Exception $e) {
           log::add('jeeasy', 'debug', 'erreur DNS > '.$e);
 		
