093c4d74b7dc4ee334ecbb01f3c5efae6dc8651b
Salvialf <jeuxalf@live.fr>
2024-08-11 17:56:04 +0200

    rewrite language (WIP for reload)

--- desktop/modal/language.php
+++ desktop/modal/language.php
@@ -3,91 +3,49 @@ if (!isConnect()) {
   throw new Exception('{{401 - Accès non autorisé}}');
 }
 
-
 $actualLanguage = config::byKey('language');
-log::add('jeeasy','debug','test '.$atualLanguage);
-
-if (file_exists(config::byKey('path_wizard')))
-  $path_wizard = json_decode(file_get_contents(config::byKey('path_wizard')), true);
-else
-  $path_wizard = json_decode(file_get_contents('plugins/jeeasy/core/data/wizard.json'), true);
-
-
-
 ?>
-  
-  <script>
-  $('#languageJeeasy option[value="<?= $actualLanguage; ?>"]').prop('selected', true);
-  
-  </script>
 
-<div class="mainContainer" style="height:100%;display:flex;flex-direction:column;justify-content:center;">
-<div class="col-md-6 col-md-offset-3 text-center"><img class="img-responsive center-block img-atlas" style="width:80%;height:80%;" src="<?php echo config::byKey('product_connection_image'); ?>" /></div>
-<div class="col-md-12 text-center">
-  <p class="text-center">
-  <h3 class="titlelanguage" id="titlelanguage">{{Langage Systeme}}</h3>
-  </p>
-  <p class="text-center">
-  <h4 class="textAtlas" style="color:#93ca02;"></h4>
-  </p>
-  
-  
-      <div style="display:flex;justify-content:center;">
-            <select class="form-control selectpicker" id="languageJeeasy" style="width:350px">
-                    <option value="fr_FR">Français</option>
-                    <option value="en_US">English</option>
-                    <option value="de_DE">Deutsch</option>
-                    <option value="es_ES">Español</option>
-                    <option value="it_IT">Italiano (nessun supporto)</option>
-                    <option value="pt_PT">Português (sem apoio)</option>
-                    <option value="ru_RU">Русский язык (без поддержки)</option>
-                    <option value="ja_JP">日本語（非対応)</option>
-                    <option value="id_ID">Bahasa Indonesia (tidak mendukung)</option>
-                    <option value="tr">Türkçe (destek yok)</option>
-            </select>
-      </div>
-
-
-  <br>
-
-
-  <div class="testbtnb" style="display:flex; flex-direction:column;justify-content:center; align-items:center;">
-    <p class="ignorebtn">{{Cliquez sur la flèche pour ignorer}}</p>
-  </div>
-
+<h3>{{Choix de la langue}}</h3>
+<img src="<?php echo config::byKey('product_connection_image'); ?>" alt="Product Image" style="max-width:100%">
+<p>
+  <strong>{{Vous pouvez changer la langue de votre box ou passer à l'étape suivante}} <i class='far fa-arrow-alt-circle-right'></i></strong>
+</p>
+<div class="input-group col-md-4 col-md-offset-4">
+  <select class="form-control roundedLeft" id="in_language" style="width:350px">
+    <option value="fr_FR" <?= ($actualLanguage == 'fr_FR') ? ' selected' : '' ?>>Français</option>
+    <option value="en_US" <?= ($actualLanguage == 'en_US') ? ' selected' : '' ?>>English</option>
+    <option value="de_DE" <?= ($actualLanguage == 'de_DE') ? ' selected' : '' ?>>Deutsch</option>
+    <option value="es_ES" <?= ($actualLanguage == 'es_ES') ? ' selected' : '' ?>>Español</option>
+    <option value="it_IT" <?= ($actualLanguage == 'it_IT') ? ' selected' : '' ?>>Italiano (nessun supporto)</option>
+    <option value="pt_PT" <?= ($actualLanguage == 'pt_PT') ? ' selected' : '' ?>>Português (sem apoio)</option>
+  </select>
+  <span class="input-group-btn">
+    <button type="button" class="btn btn-success roundedRight" id="btn_language">{{Valider}}</button>
+  </span>
 </div>
 
-
-
-  <script>
-
-  
-  $('#languageJeeasy').on('change', function() {
-        $('#bt_next').show();
-        $('.textAtlas').append($('#languageJeeasy option:selected').text());
-        $('#titlelanguage').hide();
-        $.ajax({
-          type: "POST",
-          url: "plugins/jeeasy/core/ajax/jeeasy.ajax.php",
-          data: {
-            action: "choiceLanguageJeeasy",
-            choice: $('#languageJeeasy').val()
-          },
-          dataType: 'json',
-          error: function(request, status, error) {
-            console.log(status);
-            handleAjaxError(request, status, error);
-          },
-          success: function(data) {
-            $('#md_modal').dialog({
-              title: "{{Bienvenue}}"
-            });
-            $("#md_modal").load('index.php?v=d&modal=wizard&plugin=jeeasy').dialog('open');
-          }
-        });
-    });
-  
-  
-
-
-  </script>
+<script>
+  document.getElementById('btn_language').addEventListener('click', function(_event) {
+    let newLanguage = document.getElementById('in_language').value
+    jeedom.config.save({
+      configuration: {
+        language: newLanguage
+      },
+      error: function(_error) {
+        jeedomUtils.showAlert({
+          message: _error.message,
+          level: 'danger'
+        })
+      },
+      success: function() {
+        jeedomUtils.showAlert({
+          message: '{{La langue a été changée}}',
+          level: 'success',
+          timeOut: 3000
+        })
+        document.getElementById('in_language').value = newLanguage
+      }
+    })
+  })
+</script>
