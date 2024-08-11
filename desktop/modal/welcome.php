9c054b0a8fdae9c00912143008a6ad18b74003d4
Salvialf <jeuxalf@live.fr>
2024-08-11 17:25:25 +0200

    load steps & add prev/next buttons

--- desktop/modal/welcome.php
+++ desktop/modal/welcome.php
@@ -3,10 +3,15 @@ if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
 include_file('desktop', 'jeeasy.welcome', 'css', 'jeeasy');
-// if (file_exists(config::byKey('path_wizard')))
-// 	$path_wizard = json_decode(file_get_contents(config::byKey('path_wizard')), true);
-// else
-// 	$path_wizard = json_decode(file_get_contents('plugins/jeeasy/core/data/wizard.json'), true);
+if (file_exists(config::byKey('path_wizard'))) {
+	$wizard = json_decode(file_get_contents(config::byKey('path_wizard')), true);
+} else {
+	$wizard = json_decode(file_get_contents('plugins/jeeasy/core/data/wizard.json'), true);
+}
+$steps = $wizard['trame'];
+usort($steps, function ($step1, $step2) {
+	return $step1['order'] <=> $step2['order'];
+});
 ?>
 
 <button class="btn btn-xs btn-danger" id="bt_quitJeeasyWizard"><i class="fas fa-times"></i> {{Quitter l'assistant}}</button>
@@ -18,14 +23,66 @@ include_file('desktop', 'jeeasy.welcome', 'css', 'jeeasy');
 		<p>{{Bienvenue dans l'assistant de configuration}} <?php echo config::byKey('product_name'); ?>.</p>
 		<p>{{Configurez facilement votre installation domotique en suivant les étapes de cet assistant.}}</p>
 		<strong>{{Cliquez sur la flèche pour commencer}} <i class='far fa-arrow-alt-circle-right'></i></strong>
-		<!-- <div class="arrow" onclick="window.location.href='nextpage.php'">
-			<i class='icon far fa-arrow-alt-circle-right icon_green'></i>
-		</div> -->
 	</div>
-	<div class="carousel-dots"></div>
+	<div id="jeeasy_navigation">
+		<div>
+			<i class="far fa-arrow-alt-circle-left navBtn prev hidden"></i>
+		</div>
+		<div>
+			<?php
+			foreach ($steps as $index => $step) {
+				echo '<span class="navDot' . (($index == 0) ? ' active' : '') . '" data-page="' . $step['wizard'] . '" data-title="' . $step['title'] . '">';
+				echo $index + 1;
+				echo '</span>';
+			}
+			?>
+			<div id="div_dots_tooltip"></div>
+		</div>
+		<div>
+			<i class="far fa-arrow-alt-circle-right navBtn next"></i>
+		</div>
+	</div>
 </div>
 
 <script>
+	var contentContainer = document.getElementById('jeeasy_container')
+	var tooltip = document.getElementById('div_dots_tooltip')
+
+	document.querySelectorAll('.navDot').forEach(_dot => {
+		_dot.addEventListener('mouseover', function(_event) {
+			tooltip.innerText = this.dataset.title;
+			tooltip.style.display = 'block';
+			tooltip.style.left = _event.pageX + 'px';
+			tooltip.style.top = (_event.pageY + 20) + 'px';
+		});
+
+		_dot.addEventListener('mouseout', function() {
+			tooltip.style.display = 'none';
+		});
+
+		_dot.addEventListener('mousemove', function(_event) {
+			tooltip.style.left = _event.pageX + 'px';
+			tooltip.style.top = (_event.pageY + 20) + 'px';
+		});
+
+		_dot.addEventListener('click', function() {
+			document.querySelectorAll('.navDot.active').removeClass('active')
+			this.addClass('active')
+			loadPageContent(this.dataset.page);
+		});
+	})
+
+	document.querySelectorAll('.navBtn').forEach(_navBtn => {
+		_navBtn.addEventListener('click', function() {
+			let activeNavDot = document.querySelector('.navDot.active')
+			if (this.classList.value.includes('right')) {
+				activeNavDot.nextElementSibling.triggerEvent('click')
+			} else if (this.classList.value.includes('left')) {
+				activeNavDot.previousElementSibling.triggerEvent('click')
+			}
+		})
+	})
+
 	document.getElementById('bt_quitJeeasyWizard').addEventListener('click', function() {
 		let confirm = "{{Voulez-vous vraiment quitter l'assistant de configuration?}}"
 		confirm += '<br><br>'
@@ -37,71 +94,18 @@ include_file('desktop', 'jeeasy.welcome', 'css', 'jeeasy');
 		})
 	})
 
-	const pages = [{
-			index: 1,
-			name: 'index.php?v=d&plugin=jeeasy&modal=welcome',
-			tooltip: '{{Bienvenue}}'
-		},
-		{
-			index: 2,
-			name: 'index.php?v=d&plugin=jeeasy&modal=boxName',
-			tooltip: '{{Nom de la box}}'
-		},
-		{
-			index: 3,
-			name: 'page3.php'
-		}
-	];
-
-	const carouselDots = document.querySelector('.carousel-dots');
-	const contentContainer = document.querySelector('.container');
-
-	const tooltip = document.createElement('div');
-	tooltip.className = 'modal-tooltip';
-	document.getElementById('jeeasy_wizard').appendChild(tooltip)
-
-	pages.forEach(page => {
-		const dot = document.createElement('div');
-		dot.classList.add('dot');
-		dot.dataset.page = page.name;
-		dot.innerText = page.index;
-
-		dot.addEventListener('mouseover', function(event) {
-			tooltip.innerText = page.tooltip;
-			tooltip.style.display = 'block';
-			tooltip.style.left = event.pageX + 'px';
-			tooltip.style.top = (event.pageY + 20) + 'px';
-		});
-
-		dot.addEventListener('mouseout', function() {
-			tooltip.style.display = 'none';
-		});
-
-		dot.addEventListener('mousemove', function(event) {
-			tooltip.style.left = event.pageX + 'px';
-			tooltip.style.top = (event.pageY + 20) + 'px';
-		});
-
-		dot.addEventListener('click', function() {
-			loadPageContent(this.dataset.page);
-		});
-
-		carouselDots.appendChild(dot);
-	});
-
-	function loadPageContent(page) {
-		const initialWidth = contentContainer.offsetWidth;
-		const initialHeight = contentContainer.offsetHeight;
-
-		fetch(page)
+	function loadPageContent(_page) {
+		fetch('index.php?v=d&plugin=jeeasy&modal=' + _page)
 			.then(response => response.text())
 			.then(data => {
-				if (page === 'index.php?v=d&plugin=jeeasy&modal=welcome') {
+				if (_page === 'welcome') {
+					document.querySelector('.navBtn.prev').addClass('hidden')
 					const parser = new DOMParser();
 					const doc = parser.parseFromString(data, 'text/html');
 					const newContent = doc.querySelector('.container').innerHTML;
 					contentContainer.innerHTML = newContent;
 				} else {
+					document.querySelector('.navBtn.prev').removeClass('hidden')
 					contentContainer.innerHTML = data;
 				}
 
