c43c987e150f55f994ef958c11d87fcbd052612c
Salvialf <jeuxalf@live.fr>
2024-08-13 22:53:10 +0200

    some css

--- desktop/php/wizard.php
+++ desktop/php/wizard.php
@@ -8,7 +8,7 @@ include_file('desktop', 'wizard', 'css', 'jeeasy');
 <button class="btn btn-xs btn-danger" id="bt_quitJeeasyWizard"><i class="fas fa-times"></i> {{Annuler l'assistant}}</button>
 
 <div id="jeeasy_wizard">
-	<div class="container" id="jeeasy_container">
+	<div class="container text-center" id="jeeasy_container">
 	</div>
 	<div id="jeeasy_navigation">
 		<div>
