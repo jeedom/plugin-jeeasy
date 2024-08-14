23ca2a61c09b8fd37e541657c28618bf85f9fbe4
Salvialf <jeuxalf@live.fr>
2024-08-14 15:38:54 +0200

    fix default tz

--- desktop/modal/general.php
+++ desktop/modal/general.php
@@ -12,7 +12,7 @@ if ($boxName == '') {
   $boxName = 'Jeedom ' . ucfirst(jeedom::getHardwareName());
   config::save('name', $boxName);
 }
-sendVarToJS('_timezone', config::byKey('timezone', 'Europe/Brussels'));
+sendVarToJS('_timezone', config::byKey('timezone'));
 ?>
 
 <h3>{{Paramètres généraux}}</h3>
