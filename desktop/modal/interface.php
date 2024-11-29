be034c06cf192f1e83a96c9a5fcc178a96ecf588
Salvialf <jeuxalf@live.fr>
2024-11-29 20:54:36 +0100

    add colored icons

--- desktop/modal/interface.php
+++ desktop/modal/interface.php
@@ -8,6 +8,7 @@ $themesDescription = array(
 );
 $defaultTheme = config::byKey('jeedom_theme_main');
 $alternateTheme = config::byKey('jeedom_theme_alternate');
+$coloredIcons = config::byKey('interface::advance::coloredIcons');
 ?>
 
 <h3>{{Paramètres d'affichage}}</h3>
@@ -15,6 +16,7 @@ $alternateTheme = config::byKey('jeedom_theme_alternate');
 <div class="bold">{{Vous pouvez modifier certains paramètres d'affichage de votre installation puis passer à l'étape suivante}}
     <i class="far fa-arrow-alt-circle-right"></i>
 </div>
+
 <div class="input-group <?= ($defaultTheme == $alternateTheme) ? ' hidden' : ''  ?>">
     <span class="input-group-addon roundedLeft">{{Thème}}
         <sup><i class="fas fa-question-circle" title="{{Sélectionner le thème de l'interface}}"></i></sup>
@@ -25,65 +27,38 @@ $alternateTheme = config::byKey('jeedom_theme_alternate');
     </select>
 </div>
 
+<div class="input-group">
+    <span class="input-group-addon roundedLeft">{{Couleur des icônes}}
+        <sup><i class="fas fa-question-circle" title="{{Cliquer sur le bouton pour autoriser les icônes colorées ou non}}"></i></sup>
+    </span>
+    <div class="input-group-addon flex-evenly" style="font-size:20px;height:32px;">
+        <i class="fas fa-check-circle icon_green"></i>
+        <i class="fas fa-exclamation-circle icon_orange"></i>
+        <i class="fas fa-times-circle icon_red"></i>
+        <i class="fas fa-lightbulb icon_yellow"></i>
+        <i class="fas fa-thermometer-empty icon_blue"></i>
+    </div>
+    <span class="input-group-btn">
+        <button class="btn btn-primary roundedRight" title="<?= ($coloredIcons == 1) ? '{{Ne pas colorer les icônes}}' : '{{Colorer les icônes}}' ?>" id="btn_coloredIcons" data-state="<?= $coloredIcons ?>">
+            <i class="fas fa-sync-alt"></i>
+        </button>
+    </span>
+</div>
 <script>
     jeedomUtils.initTooltips()
 
     document.querySelector('#in_theme option[value="' + document.body.dataset.theme + '"]').selected = true
-    document.getElementById('in_theme').addEventListener('change', function(_event) {
+    document.getElementById('in_theme').addEventListener('change', function() {
         jeedomUtils.switchTheme()
     })
+
+    document.getElementById('btn_coloredIcons').addEventListener('click', function() {
+        let value = (this.dataset.state == 1) ? 0 : 1
+        configSave({
+            'interface::advance::coloredIcons': value
+        })
+        this.dataset.state = value
+        this.title = (value == 1) ? '{{Ne pas colorer les icônes}}' : '{{Colorer les icônes}}'
+        document.body.setAttribute('data-coloredIcons', value)
+    })
 </script>
-
-
-<!-- <h3>{{Personnalisation de l'interface}}</h3>
-    <img src="<?php echo config::byKey('product_connection_image'); ?>" alt="Product Image">
-    <br>
-    <label>{{Icones colorées}} : </label>
-    <input type="checkbox" id="changeColoredIcons"></checkbox>
-
-
-
-
-<script>
-
-// Fonction pour eviter conflits de portée
-(function() {
-
-    let coloredIcons = '<?= config::byKey('interface::advance::coloredIcons'); ?>';
-
-    let checkboxColored = document.getElementById('changeColoredIcons');
-
-    let isInitialUpdate = true;
-
-    checkboxColored.checked = coloredIcons == '1'  ? true : false;
-
-    isInitialUpdate = false;
-
-
-    checkboxColored.addEventListener('click', function(_event) {
-        if (isInitialUpdate) {
-            return;
-        }
-
-        let newColoredIcons = checkboxColored.checked ? 1 : 0;
-        jeedom.config.save({
-            configuration: {
-                'interface::advance::coloredIcons': newColoredIcons
-            },
-            error: function(_error) {
-                jeedomUtils.showAlert({
-                    message: _error.message,
-                    level: 'danger'
-                });
-            },
-            success: function() {
-                jeedomUtils.showAlert({
-                    message: '{{Les icones colorées sont}} : <strong>' + (newColoredIcons ? 'Activées' : 'Désactivées') + '</strong>',
-                    level: 'success',
-                    timeOut: 3000
-                });
-            }
-        });
-    });
-})();
-</script> -->
