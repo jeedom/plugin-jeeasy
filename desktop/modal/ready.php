fccca25f980aaf105ec3bd1d4a9c6fc721cc6009
Salvialf <jeuxalf@live.fr>
2024-11-29 22:08:34 +0100

    remove unused

--- desktop/modal/ready.php
+++ desktop/modal/ready.php
@@ -3,51 +3,26 @@ if (!isConnect()) {
 	throw new Exception('401 - {{Accès non autorisé}}');
 }
 $wizardMode = jeeasy::getWizardMode();
-if ($wizardMode == 'recovery') {
 ?>
-	<h3>{{Félicitations}} !</h3>
-	<img src="plugins/jeeasy/core/img/greenthumb.png" alt="{{pouce de validation}}" style="width:180px;">
-	<h4>{{Vous avez terminé la phase de préparation}}</h4>
+<h3>{{Félicitations}} !</h3>
+<img src="plugins/jeeasy/core/img/greenthumb.png" alt="{{pouce de validation}}" style="width:180px;">
+<div>
+	{{Vous pouvez commencer à naviguer dans}} <?php echo config::byKey('product_name') ?> {{pendant que les plugins finissent de s'installer.}}
+	<br>
+	{{Cette opération peut prendre jusqu'à 30 minutes.}}
+</div>
+
+<div class="bold">{{Cliquez sur la coche en bas à droite}} <i class="fas fa-check-circle"></i> {{pour valider la configuration de votre installation.}}</div>
 
 <?php
-} else {
-	$jsonrpc = repo_market::getJsonRpc();
-	$servicePack = '';
-	if ($jsonrpc->sendRequest('servicepack::info')) {
-		$servicePack = $jsonrpc->getResult()['licenceName'];
-	}
+if ($wizardMode == 'default') {
 ?>
-	<h3>{{Félicitations}} !</h3>
-	<img src="plugins/jeeasy/core/img/greenthumb.png" alt="{{pouce de validation}}" style="width:180px;">
-	<div>{{Vous pouvez commencer à naviguer dans}} <?php echo config::byKey('product_name') ?>
-		{{pendant que les plugins finissent de s'installer.}}
+	<hr class="hrPrimary">
+	<div>
+		{{Retrouvez la documentation complète}} : <a href="https://www.jeedom.com/doc" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> {{Documentation}}</a>
+		<br><br>
+		{{Rejoignez la communauté}} : <a href="https://community.jeedom.com" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-users"></i> {{Forum}}</a>
 	</div>
-	<div>{{Cette opération peut prendre jusqu'à 30 minutes.}}</div>
-	<div class="bold">{{Cliquez sur la coche en bas à droite}} <i class="fas fa-check-circle"></i> {{pour valider la configuration de votre installation.}}</div>
-
-
-	<!-- <hr class="hrPrimary">
-
-	<div class="internalDiv">
-		<span>{{Accès local}} :</span>
-		<strong><?= network::getNetworkAccess('internal'); ?></strong>
-	</div>
-	<div style="display: <?= ($servicePack !== 'community') ? '' : 'none' ?>;">
-		<span>{{Accès externe}} :</span>
-		<span style="font-weight:bold;">
-			<?= (network::getNetworkAccess('external') == 'http:') ? "{{Le plugin Openvpn est en cours d installation}}" : network::getNetworkAccess('external'); ?>
-		</span>
-	</div> -->
-
-	<?php
-	if ($wizardMode == 'default') {
-	?>
-		<hr class="hrPrimary">
-
-		<div>{{Retrouvez la documentation complète}} : <a href="https://www.jeedom.com/doc" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-book"></i> {{Documentation}}</a></div>
-		<div>{{Rejoignez la communauté}} : <a href="https://community.jeedom.com" target="_blank" class="btn btn-default btn-xs" role="button"><i class="fas fa-users"></i> {{Forum}}</a></div>
-	<?php
-	}
-	?>
 <?php
 }
+?>
