887d686ec0c27a14e97a321e207e03ea5742d3a0
Salvialf <jeuxalf@live.fr>
2026-04-17 19:06:22 +0200

    clean plugin page & css

--- desktop/php/jeeasy.php
+++ desktop/php/jeeasy.php
@@ -5,40 +5,32 @@ if (!isConnect('admin')) {
 ?>
 
 <div class="row row-overflow">
-
 	<div class="col-xs-12 eqLogicThumbnailDisplay">
-
 		<div class="eqLogicThumbnailContainer">
 			<legend style="margin-bottom:50px;"><i class="fas fa-cog"></i> {{Bienvenue sur la configuration facile avec Jeeasy}}</legend>
 
 			<div class="cursor eqLogicAction logoPrimary" id="bt_jeeasyWizardV2">
-				<i class="fas fa-hat-wizard iconTableJeeasy"></i>
+				<i class="fas fa-hat-wizard"></i>
 				<br>
-				<span>{{Jeeasy V2}}</span>
-			</div>
-
-			<div class="cursor eqLogicAction logoPrimary" id="bt_jeeasyWizard">
-				<i class="fas fa-hat-wizard iconTableJeeasy"></i>
-				<br>
-				<span>{{Relancer le wizard}}</span>
+				<span>{{Démarrer l'assistant}}</span>
 			</div>
 			<div class="cursor eqLogicAction logoSecondary" id="bt_jeeasyDiscovery">
-				<i class="fas fa-wifi iconTableJeeasy"></i>
+				<i class="fas fa-wifi"></i>
 				<br>
 				<span>{{Détecter mes équipements}}</span>
 			</div>
 			<div class="cursor eqLogicAction logoSecondary" id="bt_jeeasyObjectConfiguration">
-				<i class="fas fa-home iconTableJeeasy"></i>
+				<i class="fas fa-home"></i>
 				<br>
 				<span>{{Configurer ma maison}}</span>
 			</div>
 			<div class="cursor eqLogicAction logoSecondary" id="bt_jeeasyIncludeConfiguration">
-				<i class="fas fa-plus iconTableJeeasy"></i>
+				<i class="fas fa-plus"></i>
 				<br>
 				<span>{{Ajouter un équipement}}</span>
 			</div>
 			<div class="cursor eqLogicAction logoSecondary" id="bt_jeeasyEqLogicConfiguration">
-				<i class="fas fa-wrench iconTableJeeasy"></i>
+				<i class="fas fa-wrench"></i>
 				<br>
 				<span>{{Configurer un équipement}}</span>
 			</div>
